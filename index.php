23b273e4e518007b00c803d4a2c02137beb3ef58
Samir Mejia <samir4mt@gmail.com>
2018-04-21 22:22:08 -0500

    Creacion archivos view

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	require_once "controller/controller.php";
+	require_once "controller/MDB/mdbUsuario.php";
+
+	require_once "model/rutas.php";
+
+	$mvc = new MvcController();
+	$mvc->pagina();
+
+?>
